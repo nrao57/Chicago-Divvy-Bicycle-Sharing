d5bf62fe8d8e3eb46f590425cce07852f0f7ab2d
nrao57 <raonikhil57@gmail.com>
2018-07-03 20:16:28 -0400

    First Code Commit

---
+++ code/explore.py
@@ -0,0 +1,42 @@
+#this code is to just explore the dataset with basic panda tools
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+
+data_columns = ['trip_id', 'year', 'month', 
+		'week', 'day', 'hour', 
+		'usertype', 'gender','starttime',
+		 'stoptime', 'tripduration', 'temperature',
+		 'events','from_station_id', 'from_station_name', 
+		'latitude_start','longitude_start', 'dpcapacity_start', 
+		'to_station_id','to_station_name', 'latitude_end', 
+		'longitude_end', 'dpcapacity_end'] 
+
+#lets set the directory path where all the data is located
+os.chdir('..')
+dir_path = "chicago-divvy-bicycle-sharing-data/"
+data_filename = "data.csv" 
+
+#Create an Iterator from the data
+data_set = pd.read_csv(dir_path+data_filename, chunksize=100000)
+
+#calculate total number of rows of data
+def calc_totalrows():
+	row_len_map = map(lambda x: x.shape[0], data_set)
+	row_len_list = list(row_len_map)
+	total_rows = sum(row_len_list) #returns 9495235
+ 
+
+#get dataframe of a random sample of all data 
+def get_subset(data_set):
+	data_reduced = pd.DataFrame()
+	for chunk in data_set:
+		data_reduced = pd.concat([data_reduced,chunk.sample(frac=0.001)])
+	print(data_reduced.shape)
+
+get_subset(data_set)
+
+
+
+
